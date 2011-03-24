e08758601ca760b957cde2b47e48b2a757b23e7c
Motiejus Jakštys <desired.mta@gmail.com>
2011-03-24 01:02:09 +0000

    Code and Makefile cleanup

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-git2: git2.c libgit2.a
-git2.c:
-	gcc -static src/git2.c -I. -L. -lgit2 -ogit2 -Wall -pedantic
+git2: git2.c libgit2.ready
+	gcc -static git2.c -O2 -I. -L./libgit2/build/static -o git2 -Wall -lgit2 -lz -Wall -Wextra
 
-libgit2: libgit2.a
+libgit2.ready:
+	cd libgit2 && ./waf configure build-static && cd ../ && touch libgit2.ready
 
-libgit2.a:
-	cd libgit2; ./waf configure build-static; cp build/static/libgit2.a ../
+clean:
+	rm -f git2 libgit2.ready; cd libgit2; ./waf distclean
--- git2.c
+++ git2.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "libgit2/include/git2.h"
 
-// doh ~stub
+/* doh ~stub */
 void get_master_hex(char *hex) {
     FILE *f = fopen(".git/refs/heads/master", "r");
     fgets(hex, 41, f);
@@ -9,50 +12,49 @@ void get_master_hex(char *hex) {
 }
 
 int main (int argc, char** argv) {
-    if (strcmp(argv[1], "rev-list") != 0) {
-        err(0, "Not implemented!\n");
+    if (argc >= 2 && strcmp(argv[1], "rev-list") != 0) {
+        printf("Not implemented!\n");
         exit(1);
     }
 
     if (argc != 3) {
-        err(0, "Usage: git2 rev-list <commit>\n");
+        printf("Usage: git2 rev-list <commit>\n");
         exit(1);
     }
 
-    char *hex = (char*)malloc(41*sizeof(char));
-    get_master_hex(hex); // do not look what's inside
+    char hex[41];
+    get_master_hex(hex); /* do not look what's inside */
 
     int e;
-    // Start rev-list code //
+    /* Start rev-list code */
     git_repository *repo;
-    if (e = git_repository_open(&repo, ".git") != 0)
-        err(1, "Failed git repo open: %d" , e);
+    if ((e = git_repository_open(&repo, ".git")) != 0)
+        printf("Failed git repo open: %d" , e);
 
     git_oid id;
     git_revwalk *walk;
-    git_commit *head, *commit;
+    git_commit *head;
 
     git_oid_mkstr(&id, hex);
-    //printf("Raw 20 bytes: [%s]\n", (&id)->id);
 
-    git_odb *odb; // fetch the object!
+    git_odb *odb; /* fetch the object! */
     odb = git_repository_database(repo);
 
-    if (e = git_commit_lookup(&head, repo, &id) != 0)
-        err(1, "Error in git commit lookup: %d\n");
-    if (e = git_revwalk_new(&walk, repo))
-        err(1, "Fail walker: %d\n", e);
-    //git_revwalk_sorting(walk, GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE);
-    if (e = git_revwalk_push(walk, &id))
-        err(1, "Failed revwalk push: %d\n", e);
+    if ((e = git_commit_lookup(&head, repo, &id)) != 0)
+        printf("Error in git commit lookup: %d\n", e);
+    if ((e = git_revwalk_new(&walk, repo)) != 0)
+        printf("Fail walker: %d\n", e);
+    /*git_revwalk_sorting(walk, GIT_SORT_TOPOLOGICAL); */
+    if ((e = git_revwalk_push(walk, &id)) != 0)
+        printf("Failed revwalk push: %d\n", e);
 
-    int ret = git_revwalk_next(&id, walk);
-    char *hex2 = (char*) malloc(40*sizeof(char));
+    char hex2[42];
 
     git_oid_fmt(hex2, &id);
     
     while (git_revwalk_next(&id, walk) == GIT_SUCCESS) {
         git_oid_fmt(hex2, &id);
+        hex2[41] = '\0';
         printf("%s\n", hex2);
     }
     return 0;
